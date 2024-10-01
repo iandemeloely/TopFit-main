07760ed334ec4e67ffed06e198b7a976e2e3f4e1
Gabrielli-Rech <gabbyrech133@gmail.com>
2024-10-01 10:33:01 -0300

    academia

--- src/controller/GrupoMuscularController.java
+++ src/controller/GrupoMuscularController.java
@@ -1,5 +1,33 @@
 package controller;
+import java.util.ArrayList;
 
+import dao.DAOFactory;
+import model.Academia;
+import model.GrupoMuscular;
 public class GrupoMuscularController {
-    
+        GrupoMuscularDAO iDAO = DAOFactory.GrupoMuscularDAO();
+
+    public void cadastrarGrupoMuscular(GrupoMuscular lVO) {
+        iDAO.cadastrarGrupoMuscular(lVO);
+    }
+
+    public ArrayList<GrupoMuscular> listaGrupoMuscular() {
+        return iDAO.listarGrupoMusculars();
+    }
+
+    public GrupoMuscular buscarGrupoMuscularPorNome(String nome) {
+        return iDAO.buscarGrupoMuscularPorNome(nome);
+    }
+
+    public void atualizarGrupoMuscular(GrupoMuscular lVO) {
+        iDAO.atualizarGrupoMuscular(lVO);
+    }
+
+    public boolean deletarGrupoMuscular(int id) {
+        return iDAO.deletarGrupoMuscular(id);
+    }
+
+    public Academia getGrupoMuscularPorId(int id) {
+        return iDAO.buscarGrupoMuscularPorId(id);
+    }
 }
